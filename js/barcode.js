0e6e92db5bfe910b73ee328211fe0a33faa651f7
sungnoone <sungnoone@gmail.com>
2014-10-28 11:28:58 +0800

    v0.06
    外單內容顯示

--- js/barcode.js
+++ js/barcode.js
@@ -4,13 +4,9 @@
 
 $(document).ready(function(){
 
-})
+});
 
 /*===================================================================*/
-
-
-
-
 
 
 /*===================================================================*/
@@ -36,12 +32,25 @@ function btn_test(){
 
 //新增外單 -- addorder.html
 function btn_order_save(){
-    var json_obj = JSON.stringify({"author":"王菲", "text":"我不願意!!"});
+    var Order_Shipping_Barcode = $("#order_barcode").val();
+    var Order_Shipping_Subscriber = $("#order_subscriber").val();
+    var Order_Shipping_Address = $("#order_address").val();
+
+    if(Order_Shipping_Barcode == ""){
+        alert("請輸入訂單編號！！");
+        return;
+    }
+
+    var Order_Json_Object = {};
+    Order_Json_Object[ORDER_SHIPPING_BARCODE] = Order_Shipping_Barcode;
+    Order_Json_Object[ORDER_SHIPPING_SUBSCRIBER] = Order_Shipping_Subscriber;
+    Order_Json_Object[ORDER_SHIPPING_ADDRESS] = Order_Shipping_Address;
+    var jsonText = JSON.stringify(Order_Json_Object);
+    alert(jsonText);
     $.ajax({
         type: "POST",
         url:RESTFUL_HOST+RESTFUL_URL_ORDER_SAVE,
-        data: json_obj,
-        //contentType: "application/json; charset=utf-8",
+        data: jsonText,
         contentType: "application/json",
         dataType: "text",
         success: function(data, status, jqXHR){
@@ -53,7 +62,6 @@ function btn_order_save(){
     });
 }
 
-/*===================================================================*/
 
 //新增產品 -- addbook.html
 function btn_book_save(){
@@ -62,11 +70,21 @@ function btn_book_save(){
     var Book_Price = $("#book_price").val();
     var Book_Description = $("#book_description").val();
 
-    var Book_Json_Object = JSON.stringify({"Book_Barcode":Book_Barcode,"Book_Name":Book_Name,"Book_Price":Book_Price,"Book_Description":Book_Description});
+    if(Book_Barcode=="" || Book_Name==""){
+        alert("請輸入編號及書名！！");
+        return;
+    }
+
+    var Book_Json_Object = {};
+    Book_Json_Object[BOOK_IN_STOCK_FIELD_BARCODE] = Book_Barcode;
+    Book_Json_Object[BOOK_IN_STOCK_FIELD_NAME] = Book_Name;
+    Book_Json_Object[BOOK_IN_STOCK_FIELD_PRICE] = Book_Price;
+    Book_Json_Object[BOOK_IN_STOCK_FIELD_DESCRIPTION] = Book_Description;
+    var jsonText = JSON.stringify(Book_Json_Object);
     $.ajax({
         type: "POST",
         url:RESTFUL_HOST+RESTFUL_URL_BOOK_SAVE,
-        data: Book_Json_Object,
+        data: jsonText,
         contentType: "application/json",
         dataType: "text",
         success: function(data, status, jqXHR){
@@ -80,27 +98,124 @@ function btn_book_save(){
 }
 
 
-function btn_book_pic1_upload(){
-    /*
-    var files = $("#book_pic1").prop("files");
-    for(var v1=0; v1<files.length; v1++){
-        var reader = new FileReader();
-        reader.readAsBinaryString(files[v1]);
-        alert(files[v1].name);
-    }*/
+//訂單加入書籍資料
+function add_book_into_order(){
+    var bookBarcodeStr = $("#book_barcode").val();
+    var orderBarcodeStr = $("#order_barcode").val();
+    var Query_Json_Object = {};
+    Query_Json_Object[BOOK_IN_STOCK_FIELD_BARCODE] = bookBarcodeStr;
+    Query_Json_Object[ORDER_SHIPPING_BARCODE] = orderBarcodeStr;
+    var jsonText = JSON.stringify(Query_Json_Object);
+    $.ajax({
+        type: "POST",
+        url:RESTFUL_HOST + RESTFUL_URL_ADD_ORDER_CONTENT,
+        data: jsonText,
+        contentType: "application/json",
+        dataType: "html",
+        success: function(data, status, jqXHR){
+            //insert to order content
+            $("#books_list").empty();
+            $("#books_list").append(data);
+            //re-generate books list
+        },
+        error: function(jqXHR, status){
+            //alert("FAIL!");
+        }
+    });
+}
 
-    $("#book_pic1").fileupload({
-        dataType: "text",
-        done: function(e, data){
-            $.each(data.result.files, function(index, file){
-                alert(file.name);
-                $("#img").append("<p>"+file.name+"</p>")
-            });
+
+//輸入訂單編號有異動
+function order_barcode_input_chang(){
+    var orderBarcodeStr = $("#order_barcode").val();
+    var Query_Json_Object = {};
+    Query_Json_Object[ORDER_SHIPPING_BARCODE] = orderBarcodeStr;
+    var jsonText = JSON.stringify(Query_Json_Object);
+    $.ajax({
+        type: "POST",
+        url:RESTFUL_HOST + RESTFUL_URL_ORDER_QUERY_BY_BARCODE,
+        data: jsonText,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data, status, jqXHR){
+            var count = 0;
+            $("#books_ul").empty();
+            for(var key in data){
+                count++;
+                var item = $.parseJSON(data[key]);
+                if(item){
+                    for(var key1 in item){
+                        if(key1==ORDER_SHIPPING_ADDRESS){
+                            $("#order_address").val(item[key1]);
+                        }else if(key1==ORDER_SHIPPING_SUBSCRIBER){
+                            $("#order_subscriber").val(item[key1]);
+                        }else if(key1==ORDER_SHIPPING_CONTENT){
+                            var barcode_array = item[key1];
+                            for(var v1=0; v1<barcode_array.length; v1++){
+                                query_book_by_barcode(barcode_array[v1].toString());
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        error: function(jqXHR, status){
+            alert("FAIL!");
+        }
+    });
+}
+
+
+/*=============== common(sub) function ===================*/
+
+
+function query_book_by_barcode(barcode){
+    if(barcode=="" || barcode==undefined){
+        //alert("沒有可供查詢的條碼編號！！");
+        return;
+    }
+    alert(barcode);
+    $.ajax({
+        type: "POST",
+        url:RESTFUL_HOST + RESTFUL_URL_BOOK_QUERY_BY_BARCODE,
+        data: barcode,
+        contentType: "application/text; charset=utf-8",
+        dataType: "json",
+        success: function(data, status, jqXHR){
+            //alert(data.length());
+            //var jsonObj = $.parseJSON(data);
+            for(var key in data){
+                var item = $.parseJSON(data[key]);
+                $("#books_ul").append("<li id='li_"+barcode+"'></li>");
+                $("#li_"+barcode).append("<h3>"+barcode+"</h3>");
+                if(item){
+                    for(var key1 in item){
+                        if(key1==BOOK_IN_STOCK_FIELD_NAME){
+                            $("#li_"+barcode).append("<p>"+item[key1]+"</p>");
+                        }
+                    }
+                }
+            }
+            $("#books_ul").listview("refresh");
+        },
+        error: function(jqXHR, status){
+            alert("FAIL!");
         }
     });
 
 }
 
 
-
-/*===================================================================*/
+//compareJSONKeyReturnValue() 比對JSON取值
+function compareJSONKeyReturnValue(compareString, jsonObject){
+    JSONMAPVALUE = "";
+    try{
+        jQuery.each(jsonObject, function(i, val){
+            if(i==compareString){
+                JSON_KEY_MAP_VALUE = val;
+            }
+        });
+    }catch (ex){
+        return ex.toString();
+    }
+}
--- js/index.js
+++ js/index.js
@@ -3,11 +3,26 @@
 var RESTFUL_HOST = "http://192.168.1.229:8088"; //服務主機
 var RESTFUL_URL_TEST = "/msrv2/"; //測試用
 
-var RESTFUL_URL_ORDER_SAVE = "/msrv2/order/order_insert_multi/"; //新增外單
-var RESTFUL_URL_BOOK_SAVE = "/msrv2/book/book_insert_multi/"; //新增產品基本檔
+var RESTFUL_URL_ORDER_SAVE = "/msrv2/order/order_insert/"; //新增空訂單
+var RESTFUL_URL_ORDER_QUERY_BY_BARCODE =  "/msrv2/order/order_query_by_barcode/"; //以條碼查詢訂單
+var RESTFUL_URL_BOOK_QUERY_BY_BARCODE =  "/msrv2/book/book_query_by_barcode/"; //以條碼查詢書本
+var RESTFUL_URL_ADD_ORDER_CONTENT = "/msrv2/book/book_insert_into_order/";//新增(修改)訂單內容
+
+var RESTFUL_URL_BOOK_SAVE = "/msrv2/book/book_insert/"; //新增產品基本檔
 
 var RESTFUL_URL_USER_ADD = "/msrv2/admin/account_create/"; //新增使用者帳號
 
+//BOOK_IN_STOCK fields
+var BOOK_IN_STOCK_FIELD_NAME = "book_name";
+var BOOK_IN_STOCK_FIELD_BARCODE = "book_barcode";
+var BOOK_IN_STOCK_FIELD_PRICE = "book_price";
+var BOOK_IN_STOCK_FIELD_DESCRIPTION = "book_description";
+
+//ORDER_SHIPPING fields
+var ORDER_SHIPPING_BARCODE = "order_barcode";
+var ORDER_SHIPPING_SUBSCRIBER = "order_subscriber";
+var ORDER_SHIPPING_ADDRESS = "order_address";
+var ORDER_SHIPPING_CONTENT = "order_content";
 
 /*function change_page_admin(){
     $.mobile.pageContainer.pagecontainer("change", "admin.html", {
